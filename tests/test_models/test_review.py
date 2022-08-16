4bb8f840a1dd5fb4b4034566757a8f0f7eb5d646
Amodoi Peter <amodoipeter@gmail.com>
2022-08-16 03:45:49 +0300

    tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """Unittest module for the Review Class."""
 
-import unittest
-from datetime import datetime
-import time
-from models.review import Review
-import re
-import json
-from models.engine.file_storage import FileStorage
 import os
+import unittest
+
 from models import storage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.review import Review
 
 
 class TestReview(unittest.TestCase):
